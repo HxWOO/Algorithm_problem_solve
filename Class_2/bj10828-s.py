6555fb1d3c828ba3d8bbffbbbe08b09a43cc9845
HyunWoo <kimjd766@gmail.com>
2023-09-27 15:34:56 +0900

    2023-09-27 스택(수정)

--- Class_2/bj10828-s.py
+++ Class_2/bj10828-s.py
@@ -2,29 +2,27 @@ import sys
 
 n = int(sys.stdin.readline())
 
-res = []
+act = []
 for i in range(n):
-    act = []
+
     inp = list(sys.stdin.readline().split())
+
     if inp[0] == 'push':
         act.append(inp[1])
     elif inp[0] == 'top':
-        try:
-            res.append(act[-1])
-        except:
-            res.append(-1)
+        if len(act) == 0:
+            print(-1)
+        else:
+            print(act[-1])
     elif inp[0] == 'size':
-        res.append(len(act))
+        print(len(act))
     elif inp[0] == 'pop':
-        try:
-            res.append(act.pop())
-        except:
-            res.append(-1)
+        if len(act) == 0:
+            print(-1)
+        else:
+            print(act.pop())
     elif inp[0] == 'empty':
         if len(act) == 0:
-            res.append(1)
+            print(1)
         else:
-            res.append(0)
-
-for i in res:
-    print(i)
+            print(0)
