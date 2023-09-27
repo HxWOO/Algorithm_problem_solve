1cec6c1a9c0aada215bf58428defcf49796f42cb
HyunWoo <kimjd766@gmail.com>
2023-09-27 15:53:06 +0900

    2023-09-27 덱

---
+++ Class_2/bj10866-s.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+queue = deque()
+for i in range(n):
+    order = list(sys.stdin.readline().split())
+
+    if order[0] == 'push_front':
+        queue.appendleft(order[1])
+    elif order[0] == 'push_back':
+        queue.append(order[1])
+    elif order[0] == 'pop_front':
+        try:
+            print(queue.popleft())
+        except:
+            print(-1)
+    elif order[0] == 'pop_back':
+        try:
+            print(queue.pop())
+        except:
+            print(-1)
+    elif order[0] == 'size':
+        print(len(queue))
+    elif order[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'front':
+        try:
+            print(queue[0])
+        except:
+            print(-1)
+    else:
+        try:
+            print(queue[-1])
+        except:
+            print(-1)
